ea8ccf3dd780a7596c518843a1c3b1e40c927618
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-08-29 03:09:28 +0700

    feat: add roll functionality on dice image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -46,19 +50,30 @@ fun DiceRollerApp() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
+    var result by remember { mutableStateOf(1) }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
-            contentDescription = "1"
+            painter = painterResource(imageResource), contentDescription = result.toString()
         )
 
         Spacer(modifier = Modifier.padding(16.dp))
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                result = (1..6).random()
+            },
         ) {
             Text(stringResource(id = R.string.roll))
         }
